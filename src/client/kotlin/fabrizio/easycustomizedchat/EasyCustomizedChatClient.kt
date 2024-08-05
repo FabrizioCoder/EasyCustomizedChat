210e031f38b39b36f62a31b000771c33858149a5
FabrizioCoder <fabrizioivan36@gmail.com>
2024-08-04 21:19:08 -0600

    Initial commit

---
+++ src/client/kotlin/fabrizio/easycustomizedchat/EasyCustomizedChatClient.kt
@@ -0,0 +1,9 @@
+package fabrizio.easycustomizedchat
+
+import net.fabricmc.api.ClientModInitializer
+
+object EasyCustomizedChatClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/fabrizio/easycustomizedchat/EasyCustomizedChat.kt
@@ -0,0 +1,15 @@
+package fabrizio.easycustomizedchat
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object EasyCustomizedChat : ModInitializer {
+    private val logger = LoggerFactory.getLogger("easycustomizedchat")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
